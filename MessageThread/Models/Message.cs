0f55e4828992fb0c94cfad6754d5a79017c5b459
Jenny Evertsson <jentap@gmail.com>
2025-04-02 17:55:17 +0200

    Adding created date to messages

--- MessageThread/Models/Message.cs
+++ MessageThread/Models/Message.cs
@@ -6,4 +6,5 @@ public class Message
 {
     [Key] public int MessageId { get; set; }
     public required string Note { get; set; }
+    public long Datestamp { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 }
